63dd5ac7804a8c1d5d5907c7f51a5fc201f46d05
Augusto Lopes <augustolfp@gmail.com>
2022-06-25 22:00:15 -0300

    Setup MongoDB and Express server connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express, {json} from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
+import joi from 'joi';
 dotenv.config();
 
 import { MongoClient } from 'mongodb';
+import { appendFile } from 'fs';
 
 const server = express();
 server.use(cors());
 server.use(json());
 
+let db;
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+mongoClient.connect(() => {
+    db = mongoClient.db('chat_UOL');
+});
+
+server.listen(5000);
