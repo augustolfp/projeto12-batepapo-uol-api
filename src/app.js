fc4ef016a107cdf239e566be69b67114159a015e
Augusto Lopes <augustolfp@gmail.com>
2022-06-25 22:05:32 -0300

    Creates validation Schemas using joi Module

--- src/app.js
+++ src/app.js
@@ -4,6 +4,19 @@ import cors from 'cors';
 import joi from 'joi';
 dotenv.config();
 
+const userSchema = joi.object({
+    name: joi.string().required(),
+    lastStatus: joi.number().integer().required()
+});
+
+const messageSchema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required(),
+    time: joi.string().required()
+})
+
 import { MongoClient } from 'mongodb';
 import { appendFile } from 'fs';
 
